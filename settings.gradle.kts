aca876f4d552e4c625bbf96934f40cf0746c801c
samcom <kishan@samcomtechnobrains.com>
2025-08-01 16:51:42 +0530

    jk

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,14 +6,5 @@ pluginManagement {
     }
 }
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-    repositories {
-        google()
-        mavenCentral()
-        maven { url = uri("https://jitpack.io") }
-    }
-}
-
 rootProject.name = "Addition"
  
