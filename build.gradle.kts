b9b775f248db240776128a0572bdfb5f3a1de930
samcom <kishan@samcomtechnobrains.com>
2025-08-01 17:47:30 +0530

    dsds

--- build.gradle.kts
+++ build.gradle.kts
@@ -54,7 +54,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.kishanSamcon"
             artifactId = "addition-library"
-            version = "1.0.8"
+            version = "1.0.9"
 
             afterEvaluate {
                 from(components["release"])
